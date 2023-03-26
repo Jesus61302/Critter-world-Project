fd55dba1824a7fa0d4077c5ff4b3d75f62f06912
Jesus61302 <jesushernandez61302@gmail.com>
2023-03-26 18:31:56 -0500

    created CritterShapes class

---
+++ src/assignment5/CritterShapes.java
@@ -0,0 +1,20 @@
+package assignment5;
+
+public class CritterShapes {
+    public void circle(){
+        //todo
+    }
+    public void square(){
+        //todo
+    }
+    public void triangle(){
+        //todo
+    }
+    public void Diamond(){
+        //todo
+    }
+
+    public void star(){
+        //todo
+    }
+}
